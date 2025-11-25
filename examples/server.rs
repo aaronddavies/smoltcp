dd4662d804cf1e233d6feffcccfa19adba905b28
Aaron Davies <adavies@anduril.com>
2025-11-24 19:09:36 -0800

    bind server socket to same ident

--- examples/server.rs
+++ examples/server.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 use smoltcp::iface::{Config, Interface, SocketSet};
 use smoltcp::phy::{Device, Medium, wait as phy_wait};
 use smoltcp::socket::icmp;
-use smoltcp::time::Instant;
+use smoltcp::time::{Duration, Instant};
 use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
 
 fn main() {
@@ -41,13 +41,21 @@ fn main() {
             .push(IpCidr::new(server_address, 24))
             .unwrap();
     });
+    iface.set_any_ip(true);
 
     // Create sockets
     let rx_buffer = icmp::PacketBuffer::new(vec![icmp::PacketMetadata::EMPTY], vec![0; 2048]);
     let tx_buffer = icmp::PacketBuffer::new(vec![icmp::PacketMetadata::EMPTY], vec![0; 2048]);
     let socket = icmp::Socket::new(rx_buffer, tx_buffer);
+
     let mut sockets = SocketSet::new(vec![]);
     let handle = sockets.add(socket);
+
+    let socket = sockets.get_mut::<icmp::Socket>(handle);
+    let ident = 0x1234;
+    assert_eq!(socket.bind(icmp::Endpoint::Ident(ident)), Ok(()));
+
+    debug!("Listening...");
 
     loop {
         let timestamp = Instant::now();
@@ -60,6 +68,6 @@ fn main() {
             debug!("Received to {:x?} ICMPv4 packet {:x?}", dest_addr, icmp_reply);
         }
 
-        phy_wait(fd, iface.poll_delay(timestamp, &sockets)).expect("wait error");
+        phy_wait(fd, Some(Duration::from_millis(1000))).expect("wait error");//iface.poll_delay(timestamp, &sockets)).expect("wait error");
     }
 }
